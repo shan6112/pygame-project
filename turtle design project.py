49f50f98305dabdbc81659d2153e822475859e91
shan6112 <shan6112@baysidehighschool.org>
2017-03-20 15:21:29 -0400

    Add files via upload

---
+++ turtle design project.py
@@ -0,0 +1,157 @@
+import turtle
+bob = turtle.Turtle()
+sk = turtle.Turtle()
+sk.speed(11)
+turtle.bgcolor()
+bob.speed(11)
+from m import*
+
+jump(bob,-200,200)
+
+bob.begin_fill() 
+for time in range(6):
+    
+    bob.color('blue')
+    bob.forward(100)
+    bob.right(60)
+bob.end_fill()
+
+jump(sk,-200,200)
+
+bob.begin_fill()
+
+for time in range(3):
+    
+    bob.color('black')
+    bob.forward(100)
+    bob.right(120)
+bob.end_fill()
+
+jump(bob,-50,100)
+bob.begin_fill() 
+for time in range(6):
+
+    bob.color('yellow')
+    bob.forward(100)
+    bob.right(60)
+bob.end_fill()
+
+jump(sk,-200,200)
+
+bob.begin_fill()
+
+for time in range(3):
+    
+    bob.color('black')
+    bob.forward(100)
+    bob.right(120)
+bob.end_fill()
+
+
+jump(bob,100,200)
+bob.begin_fill() 
+for time in range(6):
+
+    bob.color('blue')
+    bob.forward(100)
+    bob.right(60)
+bob.end_fill()
+
+jump(sk,-200,200)
+
+bob.begin_fill()
+
+for time in range(3):
+    
+    bob.color('black')
+    bob.forward(100)
+    bob.right(120)
+bob.end_fill()
+
+
+jump(bob,-50,-100)
+bob.begin_fill() 
+for time in range(6):
+
+    bob.color('blue')
+    bob.forward(100)
+    bob.right(60)
+bob.end_fill()
+
+jump(sk,-200,200)
+
+bob.begin_fill()
+
+for time in range(3):
+    
+    bob.color('black')
+    bob.forward(100)
+    bob.right(120)
+bob.end_fill()
+
+
+jump(bob,-200,0)
+bob.begin_fill() 
+for time in range(6):
+
+    bob.color('blue')
+    bob.forward(100)
+    bob.right(60)
+bob.end_fill()
+
+jump(sk,-200,200)
+
+bob.begin_fill()
+
+for time in range(3):
+    
+    bob.color('black')
+    bob.forward(100)
+    bob.right(120)
+bob.end_fill()
+
+
+
+jump(bob,100,0)
+bob.begin_fill() 
+for time in range(6):
+
+    bob.color('blue')
+    bob.forward(100)
+    bob.right(60)
+bob.end_fill()
+
+jump(sk,-200,200)
+
+bob.begin_fill()
+
+for time in range(3):
+    
+    bob.color('black')
+    bob.forward(100)
+    bob.right(120)
+bob.end_fill()
+
+
+jump(bob,-50,300)
+bob.begin_fill() 
+for time in range(6):
+
+    bob.color('blue')
+    bob.forward(100)
+    bob.right(60)
+bob.end_fill()
+
+jump(sk,-200,200)
+
+bob.begin_fill()
+
+for time in range(3):
+    
+    bob.color('black')
+    bob.forward(100)
+    bob.right(120)
+bob.end_fill()
+
+
+
